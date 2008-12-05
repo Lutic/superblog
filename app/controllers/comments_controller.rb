9f562ca54b244aa8bc34baf96c67fc9be9614047
Alexey Nayden <alexniden@gmail.com>
2008-12-05 22:18:26 +0300

    Комментарии к постам вернулись к начальному состоянию (готово к завтрашнему занятию)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,4 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new params[:comment]
-    if @comment.save
-      redirect_to 
-    else
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,6 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
   
-  map.resources :posts, :has_many => [:comments]
+  map.resources :posts, :has_many => :comments
   map.resources :comments
 end
